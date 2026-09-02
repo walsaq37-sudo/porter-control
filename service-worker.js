4bbdf5f04dbc032b39df595ce7b21a26a4bd6b2a
walsaq37-sudo <walsaq37@gmail.com>
2026-09-02 08:50:43 +0100

    Refresh Porter Control cache for flexible rounds

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='porter-control-v2';
+const CACHE='porter-control-v3';
 const ASSETS=['./','index.html','manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));

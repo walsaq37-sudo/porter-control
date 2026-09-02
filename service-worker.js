b98e057a9c08c090a9e1b62ecee14f8cb48abdd1
walsaq37-sudo <walsaq37@gmail.com>
2026-09-02 09:59:33 +0100

    Refresh Porter Control cache for redesigned UI

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='porter-control-v3';
+const CACHE='porter-control-v4';
 const ASSETS=['./','index.html','manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));

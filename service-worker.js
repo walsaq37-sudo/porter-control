f520510f437214c921a635b413e7dbcf2ace3c07
walsaq37-sudo <walsaq37@gmail.com>
2026-09-04 12:40:45 +0100

    Refresh Porter Control team version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='porter-control-v14';
+const CACHE='porter-control-v15';
 const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));

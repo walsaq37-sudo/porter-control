25ada6a764ec0401a60a0ea76800155529754e21
walsaq37-sudo <walsaq37@gmail.com>
2026-09-04 18:00:53 +0100

    Load shared container checks module

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='porter-control-v20';
-const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg','shared-inspections.js','shared-missing.js'];
+const CACHE='porter-control-v21';
+const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg','shared-inspections.js','shared-missing.js','shared-containers.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
@@ -10,6 +10,7 @@ self.addEventListener('fetch',e=>{
       let html=await r.text();
       if(!html.includes('shared-inspections.js'))html=html.replace('</body>','<script src="shared-inspections.js?v=1"></script></body>');
       if(!html.includes('shared-missing.js'))html=html.replace('</body>','<script src="shared-missing.js?v=1"></script></body>');
+      if(!html.includes('shared-containers.js'))html=html.replace('</body>','<script src="shared-containers.js?v=1"></script></body>');
       return new Response(html,{status:r.status,statusText:r.statusText,headers:{'content-type':'text/html; charset=utf-8'}});
     }).catch(()=>caches.match('./')));
     return;

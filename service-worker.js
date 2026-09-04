78c9f3bb41e542b05feabb0d5e41939d85f21c54
walsaq37-sudo <walsaq37@gmail.com>
2026-09-04 11:03:49 +0100

    Refresh cache for milk product photos

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='porter-control-v8';
-const ASSETS=['./','index.html','manifest.json'];
+const CACHE='porter-control-v9';
+const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));

5667576d93d42ed584ee80367495575725db73f0
walsaq37-sudo <walsaq37@gmail.com>
2026-09-04 17:40:29 +0100

    Refresh cache for shared daily rounds

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='porter-control-v17';
+const CACHE='porter-control-v18';
 const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));

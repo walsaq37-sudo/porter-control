6d102afebeb0b92ab25b592a73ef20c834d1132e
walsaq37-sudo <walsaq37@gmail.com>
2026-09-04 19:56:45 +0100

    Enable individual PIN login

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='porter-control-v30';
-const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg','shared-inspections.js','shared-missing.js','shared-containers.js','team-shifts.js','supervisory-view.js','team-messages.js'];
+const CACHE='porter-control-v31';
+const ASSETS=['./','index.html','manifest.json','milk-semi.svg','milk-oat.svg','shared-inspections.js','shared-missing.js','shared-containers.js','team-shifts.js','supervisory-view.js','team-messages.js','team-login.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
@@ -14,6 +14,7 @@ self.addEventListener('fetch',e=>{
       if(!html.includes('team-shifts.js'))html=html.replace('</body>','<script src="team-shifts.js?v=2"></script></body>');
       if(!html.includes('supervisory-view.js'))html=html.replace('</body>','<script src="supervisory-view.js?v=4"></script></body>');
       if(!html.includes('team-messages.js'))html=html.replace('</body>','<script src="team-messages.js?v=1"></script></body>');
+      if(!html.includes('team-login.js'))html=html.replace('</body>','<script src="team-login.js?v=1"></script></body>');
       return new Response(html,{status:r.status,statusText:r.statusText,headers:{'content-type':'text/html; charset=utf-8'}});
     }).catch(()=>caches.match('./')));
     return;
